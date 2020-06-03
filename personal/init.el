09f8a12d63e6024fb74b596b47c05b829c0c9eca
Max Froumentin <github@lapin-bl.eu>
2020-06-03 14:24:17 +0100

    Tweaks

--- personal/init.el
+++ personal/init.el
@@ -1,4 +1,15 @@
-(prelude-require-packages '(rich-minority))
+(server-start)
 
+
+;; Hide list of minor modes
+(prelude-require-packages '(rich-minority))
 (rich-minority-mode 1)
 (setf rm-blacklist "")
+
+(define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map)
+
+(global-set-key "\C-s" 'isearch-forward)
+(global-set-key (kbd "<C-right>") 'right-word)
+(global-set-key (kbd "<C-left>") 'left-word)
+
+(global-display-line-numbers-mode 1)
